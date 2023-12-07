7ea42f6c4868357afcf713473e224a3d993760ff
Cristian Mendieta <cjmm0811@gmail.com>
2023-12-07 11:19:40 +0100

    Seguridad con Keycloak

---
+++ gateway/src/main/java/api_gateway/gateway/configuration/SecurityConfig.java
@@ -0,0 +1,21 @@
+package api_gateway.gateway.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
